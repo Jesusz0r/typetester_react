2090d8b5f2491c4a70e2931a07c808338e4e6079
Jesus Mendoza <jesusreveron1990@gmail.com>
2017-05-04 16:24:06 +0200

    added textalign, lineheight, fontsize functionalities

--- src/component/FontComponent.js
+++ src/component/FontComponent.js
@@ -6,17 +6,31 @@ const FontComponent = (props) => {
 	const renderFont = () => {
 		return fonts.map((font, index) => {
 			return (
-				<div className="individual-font" key={font.styles.fontFamily} style={font.styles}>
+				<div className="individual-font" key={font.styles.fontFamily}>
 					<div className="editor">
-						<i onClick={() => props.makeBold(index, font.styles.fontFamily)}>Editor</i>
+						<i onClick={() => props.makeBold(index)}>Bold</i>
+						<i onClick={() => props.makeItalic(index)}>Italic</i>
+						<i onClick={() => props.textAlign(index, 'center')}>Center</i>
+						<i onClick={() => props.textAlign(index, 'left')}>Left</i>
+						<i onClick={() => props.textAlign(index, 'right')}>Right</i>
+						<i>
+							Font Size
+							<input type="range" value={font.styles.fontSize} min="12" max="120" onChange={(e) => props.textSize(e, index)}/>
+						</i>
+						<i>
+							Line Height
+							<input type="range" value={font.styles.lineHeight} min="1" max="2" step="0.05" onChange={(e) => props.lineHeight(e, index)}/>
+						</i>
 					</div>
 
 					<h3>{font.styles.fontFamily}</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere felis sit amet
+					<p style={font.styles}>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere felis sit amet
 						vehicula congue. Aliquam maximus maximus ante vel varius. Suspendisse at urna nec arcu ultrices
 						ultricies. Etiam sed libero nec diam viverra maximus. Phasellus hendrerit eros volutpat sodales
 						sagittis. Cras eget arcu ac velit elementum scelerisque sed in ante. Morbi ut dolor interdum,
-						porta nisi at, rhoncus nisi.</p>
+						porta nisi at, rhoncus nisi.
+					</p>
 				</div>
 			);
 		});
--- src/containers/FontContainer.js
+++ src/containers/FontContainer.js
@@ -13,7 +13,8 @@ class FontContainer extends Component {
 						'fontSize': 12,
 						'fontWeight': '400',
 						'fontStyle': 'normal',
-						'fontVariant': 'normal'
+						'fontVariant': 'normal',
+						'lineHeight': 1
 					}
 				},
 				{
@@ -22,7 +23,8 @@ class FontContainer extends Component {
 						'fontSize': 12,
 						'fontWeight': '400',
 						'fontStyle': 'normal',
-						'fontVariant': 'normal'
+						'fontVariant': 'normal',
+						'lineHeight': 1
 					}
 				},
 				{
@@ -31,13 +33,18 @@ class FontContainer extends Component {
 						'fontSize': 12,
 						'fontWeight': '400',
 						'fontStyle': 'normal',
-						'fontVariant': 'normal'
+						'fontVariant': 'normal',
+						'lineHeight': 1
 					}
 				}
 			]
 		};
 
 		this._makeBold = this._makeBold.bind(this);
+		this._makeItalic = this._makeItalic.bind(this);
+		this._textAlign = this._textAlign.bind(this);
+		this._textSize = this._textSize.bind(this);
+		this._lineHeight = this._lineHeight.bind(this);
 	}
 
 	render() {
@@ -45,15 +52,66 @@ class FontContainer extends Component {
 			<div className="font-container">
 				<h1>Font Container</h1>
 
-				<FontComponent fonts={this.state.fonts} makeBold={this._makeBold} />;
+				<FontComponent
+					fonts={this.state.fonts}
+					makeBold={this._makeBold}
+					textAlign={this._textAlign}
+					makeItalic={this._makeItalic}
+					textSize={this._textSize}
+					lineHeight={this._lineHeight}
+				/>;
 			</div>
 		);
 	}
 
 	_makeBold(index) {
 		const newState = [].concat(this.state.fonts);
-		const newStyle = Object.assign({}, newState[index].styles);
+		const newStyle = Object.assign({}, this.state.fonts[index].styles);
 		newStyle.fontWeight === '400' ? newStyle.fontWeight = '700' : newStyle.fontWeight = '400';
+		newState[index].styles = newStyle;
+
+		this.setState({
+			fonts: newState
+		});
+	}
+
+	_makeItalic(index) {
+		const newState = [].concat(this.state.fonts);
+		const newStyle = Object.assign({}, this.state.fonts[index].styles);
+		newStyle.fontStyle === 'normal' ? newStyle.fontStyle = 'italic' : newStyle.fontStyle = 'normal';
+		newState[index].styles = newStyle;
+
+		this.setState({
+			fonts: newState
+		});
+	}
+
+	_textAlign(index, direction) {
+		const newState = [].concat(this.state.fonts);
+		const newStyle = Object.assign({}, this.state.fonts[index].styles);
+		newStyle.textAlign === direction ? newStyle.textAlign = 'left' : newStyle.textAlign = direction;
+		newState[index].styles = newStyle;
+
+		this.setState({
+			fonts: newState
+		});
+	}
+
+	_textSize(e, index) {
+		const newState = [].concat(this.state.fonts);
+		const newStyle = Object.assign({}, this.state.fonts[index].styles);
+		newStyle.fontSize = Number(e.target.value);
+		newState[index].styles = newStyle;
+
+		this.setState({
+			fonts: newState
+		});
+	}
+
+	_lineHeight(e, index) {
+		const newState = [].concat(this.state.fonts);
+		const newStyle = Object.assign({}, this.state.fonts[index].styles);
+		newStyle.lineHeight = Number(e.target.value);
 		newState[index].styles = newStyle;
 
 		this.setState({
